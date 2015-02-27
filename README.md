8bdf52281a8c7038357fe4d2713e1dc69f6b32ae
Mads Ulsø Østergaard <mads@madsmads.dk>
2015-02-27 16:34:59 +0100

    First major release

--- README.md
+++ README.md
@@ -42,6 +42,10 @@ gulp.task('default', ['checkout', '...', '...', '...']);
 I was tired of having to checkout files that were going to be manipulated manually.
 The npm package 'tfs-unlock' can't parse a directory, so I wrote this little plugin that can parse a directory, and performs the tfs-task on each file.
 
+## Roadmap / help wanted
+- I'd like to reach a higher coverage score, but I'm really lost when it comes to unit-testing.
+If anyone wants to contribute with some unit-testing, I'd be really happy.
+
 ## License
 [MIT](https://tldrlegal.com/license/mit-license)
 
--- test/test.js
+++ test/test.js
@@ -1,25 +1,24 @@
 var assert = require('assert');
 var tfs = require('tfs-unlock');
-var File = require('vinyl');
+var gutil = require('gulp-util');
+var through = require('through2');
 var prefixer = require('../');
 
 describe('gulp-tfs-checkout', function(){
-  describe('in buffer mode', function(){
-    it('should return true if value is a string', function(done){
-      
-      var fakeFile = new File({
-      	path: 'this/is/the/path'
-      });
-
-      var testPrefixer = prefixer();
-
-      testPrefixer.write(fakeFile);
-
-      testPrefixer.once('data', function(file) {
-      	assert.equal(typeof(file.path), 'string');
-      	done();
-      });
-
+  it('should return true if file is empty', function(done){
+    
+    var fakeFile = new gutil.File({
+      path: '/this/file/is/empty'
     });
+
+    var testPrefixer = prefixer();
+
+    testPrefixer.write(fakeFile);
+
+    testPrefixer.once('data', function(file) {
+      assert.equal(file.isNull(), true);
+      done();
+    });
+
   });
 });
